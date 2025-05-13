91a22b9008fb531b09b11fddfa18fb517978e2b1
koeyismyusername <koey2182@gmail.com>
2025-05-13 11:05:01 +0900

    Initial Commit

---
+++ src/main/java/com/koey/anonnity_api_spring_boot/AnonnityApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.koey.anonnity_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonnityApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonnityApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koey/anonnity_api_spring_boot/AnonnityApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koey.anonnity_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonnityApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
