5b62cd5bd5fe8afa444a8c9a49d0ab2dab51005d
koeyismyusername <koey2182@gmail.com>
2025-05-14 15:36:14 +0900

    🔨refactor(member_role): MemberRole 엔티티 Getter추가, 제약조건 추가
    * UNIQUE(member_id, role_id) 제약조건을 추가했습니다.
    * Getter를 추가했습니다.
    * equals, hashCode 메서드를 추가했습니다.

--- src/main/java/com/koey/anonnity_api_spring_boot/entities/MemberRole.java
+++ src/main/java/com/koey/anonnity_api_spring_boot/entities/MemberRole.java
@@ -2,8 +2,13 @@ package com.koey.anonnity_api_spring_boot.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
-@Table(name = "member_roles")
+@Table(
+        name = "member_roles",
+        uniqueConstraints = @UniqueConstraint(name = "unq_member_role", columnNames = {"member_id", "role_id"})
+)
 public class MemberRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +19,27 @@ public class MemberRole {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MemberRole that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
