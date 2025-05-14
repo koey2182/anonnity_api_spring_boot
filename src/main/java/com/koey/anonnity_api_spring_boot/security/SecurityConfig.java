11adc0239ebb11186442f699cf33244123bc3d09
koeyismyusername <koey2182@gmail.com>
2025-05-14 15:14:14 +0900

    🔧chore(security): Spring Security 설정
    * Spring Security 의존성을 추가했습니다.
    * Spring Security 설정을 추가했습니다.

---
+++ src/main/java/com/koey/anonnity_api_spring_boot/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.koey.anonnity_api_spring_boot.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtHandler), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(new GlobalExceptionFilter(), JwtAuthenticationFilter.class)
+                ;
+
+        return http.build();
+    }
+}
