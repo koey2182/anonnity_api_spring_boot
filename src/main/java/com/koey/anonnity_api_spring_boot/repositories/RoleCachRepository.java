2a5acc25cc0921f2faefd5ecb676b1fffef3bf06
koeyismyusername <koey2182@gmail.com>
2025-05-14 17:44:52 +0900

    ✨feat(roleCacheRepository): RoleCacheRepository 생성
    * roles 테이블의 데어트는 거의 변하지 않으므로 roles 테이블의 데이터를
      캐시할 수 있는 레파지토리를 구현했습니다.

---
+++ src/main/java/com/koey/anonnity_api_spring_boot/repositories/RoleCachRepository.java
@@ -0,0 +1,38 @@
+package com.koey.anonnity_api_spring_boot.repositories;
+
+import com.koey.anonnity_api_spring_boot.cache.Cache;
+import com.koey.anonnity_api_spring_boot.entities.Role;
+import com.koey.anonnity_api_spring_boot.entities.RoleType;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class RoleCachRepository {
+    private final Cache<String, Role> roleCache;
+    private final RoleRepository roleRepository;
+
+    public RoleCachRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+        this.roleCache = new Cache<>();
+    }
+
+    @PostConstruct
+    public void initRoleCache() {
+        var roles = this.roleRepository.findAll().stream()
+                .collect(Collectors.toMap(Role::getName, x -> x));
+        this.roleCache.putAll(roles);
+    }
+
+    public Role getMemberRole() {
+        var roleMemberName = RoleType.Member.getName();
+        return this.roleCache.get(roleMemberName)
+                .orElseGet(() -> {
+                    var role = this.roleRepository.findByName(roleMemberName)
+                            .orElseThrow(() -> new RuntimeException("roles 테이블에 존재 하지 않는 name으로 조회를 시도했습니다."));
+                    this.roleCache.put(role.getName(), role);
+                    return role;
+                });
+    }
+}
